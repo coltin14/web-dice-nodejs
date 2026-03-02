095eccbd1cfd77ca2b4c39d106f90f365dd49300
coltin14 <coltpayton34@yahoo.com>
2026-03-01 20:06:15 -0600

    Updated for CORS failure

--- index.js
+++ index.js
@@ -4,7 +4,19 @@ const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+app.get('/roll-no-cors/:num', (req, res) => {
+  const num = parseInt(req.params.num)
+  const rolls = []
+
+  for (let i = 0; i < num; i++) {
+	rolls.push(crypto.randomInt(1, 7)) // gives 1–6
+  }
+
+  res.json({ rolls })
+})
+
+app.use(cors({
+  origin: 'https://happy-tree-01d1df610.4.azurestaticapps.net'}))
 app.use(express.json())
 
 // Serve files from the public folder (like index.html)
